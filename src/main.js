9a352f3a65d12f0f5f1385b3d119e46c221a7eb5
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-24 22:35:23 -0600

    Add innerText to save button

--- src/main.js
+++ src/main.js
@@ -5,15 +5,19 @@ var ideaTitle = document.querySelector('.card-idea-title');
 var ideaText = document.querySelector('.card-idea-text');
 var bubbleParent = document.querySelector('.purple-1');
 
+var titles = [];
+var texts = [];
+
 bubbleParent.addEventListener('click', clickHandler)
 
 function clickHandler(event) {
-  if (event.target.className === "save-button") doyourfucntion()
+  if (event.target.className === "save-button") saveIdea()
 }
 
 function saveIdea() {
-  
+  ideaTitle.innerText = titleInput.value;
+  ideaText.innerText = bodyInput.value;
 }
 
 
-stop
+//stop

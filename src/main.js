51d0b4d78ba19e5bd8716a386f069d8ac69a3e72
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-24 22:21:08 -0600

    Add querySelectors for saving

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
+var titleInput = document.querySelector('.title-input');
+var bodyInput = document.querySelector('.body-input');
+var saveButton = document.querySelector('.save-button');
+var ideaTitle = document.querySelector('.card-idea-title');
+var ideaText = document.querySelector('.card-idea-text');
+var bubbleParent = document.querySelector('.purple-1');
 
+bubbleParent.addEventListener('click'; )
+
+
+
+
+stop

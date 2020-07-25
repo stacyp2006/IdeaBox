20216b8db8a548b2db1e952a2049772ce1453e71
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-24 22:26:39 -0600

    Add clickHandler for bublen

--- src/main.js
+++ src/main.js
@@ -5,9 +5,15 @@ var ideaTitle = document.querySelector('.card-idea-title');
 var ideaText = document.querySelector('.card-idea-text');
 var bubbleParent = document.querySelector('.purple-1');
 
-bubbleParent.addEventListener('click'; )
+bubbleParent.addEventListener('click', clickHandler)
 
+function clickHandler(event) {
+  if (event.target.className === "save-button") doyourfucntion()
+}
 
+function saveIdea() {
+  
+}
 
 
 stop

fae36e6356bb98a5edad5bc1940e60b74de6c9af
Michael Walker <Michael.e.walker.87@gmail.com>
2020-07-26 22:38:41 -0600

    LocalStroage runs when the object is empty

--- src/idea.js
+++ src/idea.js
@@ -6,13 +6,24 @@ class Idea {
     this.star = false;
   }
   saveToStorage() { //we want this to stringify the object item and set to local storage
-    var idString = this.id
+    //var idString = this.id
+    console.log(localStorage.getItem("ideas"))
+    if (localStorage.getItem("ideas") !== null){
+      var ideas = {};
+    } else {
     var ideas = JSON.parse(localStorage.getItem("ideas"));
-    // var ideas = {};
+    }
+    //var ideas = {};
+    // if (ideas === undefined) {
+    //   ideas = {};
+    // } else {
+    //   JSON.parse(localStorage.getItem("ideas"));
+    // }
     ideas[this.id] = this
     var stringifiedIdea = JSON.stringify(ideas);
     localStorage.setItem("ideas", stringifiedIdea);
   }
 }
-//if ideas is  undefined then {} 
+//if ideas is  undefined then {}
 //ls {ideas {this.id newIdeaObject}}
+//title, body
